f26e3ae3617d4fa29d8370c9aa093c9016922202
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-03 10:15:44 +0700

    feat:/ select all authors

--- author_management/internal/repository/author.go
+++ author_management/internal/repository/author.go
@@ -13,6 +13,7 @@ import (
 
 type AuthorRepository interface {
 	SelectOneAuthor(ctx context.Context, author entity.Author) (*entity.Author, error)
+	SelectAllAuthor(ctx context.Context) ([]entity.Author, error)
 }
 
 type authorRepositoryImpl struct {
@@ -73,4 +74,39 @@ func (r *authorRepositoryImpl) SelectOneAuthor(ctx context.Context, author entit
 		return nil, err
 	}
 	return &scan, nil
+}
+
+
+func (r *authorRepositoryImpl) SelectAllAuthor(ctx context.Context) ([]entity.Author, error) {
+	q := `
+		SELECT a.author_id,a.author_name,a.photo_url,a.gender,g.author_genre_id,g.genre_name 
+		FROM authors 
+		JOIN author_genres ON a.author_genre_id=g.author_genre_id
+		WHERE deleted_at is null
+		`
+	rows, err := r.db.QueryContext(ctx, q)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+	results := make([]entity.Author, 0)
+	for rows.Next() {
+		var scan entity.Author
+		if err := rows.Scan(&scan.Id, &scan.Name, &scan.PhotoUrl, &scan.Gender, &scan.Genre.Id, &scan.Genre.Name); err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+
+		results = append(results, scan)
+	}
+
+	if err := rows.Err(); err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	return results, nil
+
 }
