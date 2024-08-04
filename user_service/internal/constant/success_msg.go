b1238021e70e3cc9b7b46d4fc4307e4441cf2465
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-04 17:25:57 +0700

    feat:/add login success msg

--- user_service/internal/constant/success_msg.go
+++ user_service/internal/constant/success_msg.go
@@ -1,9 +1,10 @@
 package constant
 
 const (
-	DataRetrievedMsg = "data has been retrieved"
-	DataCreatedMsg   = "data has been created"
-	DataUpdatedMsg   = "data has been updated"
-	DataDeletedMsg   = "data has been deleted"
-	RegisterSuccessMsg = "User has succesfully register"
+	DataRetrievedMsg   = "data has been retrieved"
+	DataCreatedMsg     = "data has been created"
+	DataUpdatedMsg     = "data has been updated"
+	DataDeletedMsg     = "data has been deleted"
+	RegisterSuccessMsg = "user has succesfully register"
+	LoginPassedMsg     = "login success"
 )
