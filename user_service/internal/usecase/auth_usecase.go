3d8b958f3230047ec341306882e8f649eb3ddf54
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-04 14:29:02 +0700

    feat:add hashing password at usecase

--- user_service/internal/usecase/auth_usecase.go
+++ user_service/internal/usecase/auth_usecase.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"context"
+	"user-service/internal/constant"
 	"user-service/internal/entity"
 	"user-service/internal/repository"
+	"user-service/pkg/utils"
 )
 
 type AuthUsecase interface {
@@ -22,6 +24,11 @@ func NewAuthUsecase(userRepository repository.UserRepository) *authUsecaseImpl {
 
 func (u *authUsecaseImpl) Register(ctx context.Context, user entity.User) error {
 	_, err := u.userRepository.SelectOneByEmail(ctx, user)
+	if err != nil {
+		return err
+	}
+	user.PhotoURL = constant.DefaultPhotoURL
+	user.Password, err = utils.HashPasswordDefault(user.Password)
 	if err != nil {
 		return err
 	}
