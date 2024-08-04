49184f070f879a5dd2efb591e16b3a3dc47d103e
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-04 15:55:41 +0700

    feat:/ add error

--- user_service/internal/apperror/utils.go
+++ user_service/internal/apperror/utils.go
@@ -3,10 +3,16 @@ package apperror
 import "net/http"
 
 var errStatusCode = map[error]int{
-	ErrResourceNotFound: http.StatusBadRequest,
-	ErrInvalidRequest:   http.StatusBadRequest,
-	ErrNoRoute:          http.StatusNotFound,
-	ErrUnauthorized:     http.StatusUnauthorized,
+	ErrResourceNotFound:  http.StatusBadRequest,
+	ErrInvalidRequest:    http.StatusBadRequest,
+	ErrNoRoute:           http.StatusNotFound,
+	ErrUnauthorized:      http.StatusUnauthorized,
+	ErrInvalidCredential: http.StatusBadRequest,
+	ErrInvalidToken:      http.StatusBadRequest,
+	ErrInvalidEmail:      http.StatusBadRequest,
+	ErrInvalidPassword:   http.StatusBadRequest,
+	ErrInvalidPassToken:  http.StatusBadRequest,
+	ErrInvalidParam:      http.StatusBadRequest,
 }
 
 func GetErrStatusCode(sourceErr error) (int, error) {
