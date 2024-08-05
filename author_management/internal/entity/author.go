6662ed35a27abcb81a96801a14a75c4530aeeb05
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-06 02:40:49 +0700

    feat:/delete author genre

--- author_management/internal/entity/author.go
+++ author_management/internal/entity/author.go
@@ -1,9 +1,8 @@
 package entity
 
 type Author struct {
-	Id          uint
-	Name        string
-	PhotoUrl    string
-	Gender      string
-	AuthorGenre *AuthorGenre
+	Id       uint
+	Name     string
+	PhotoUrl string
+	Gender   string
 }
