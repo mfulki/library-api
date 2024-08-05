d219c6106c9ed40c9b26452dfde2a874f8002dea
muhammad fulki <muhammad.fulki@gmail.com>
2024-08-05 16:19:04 +0700

    feat:/update protobuf

--- book_management/internal/pb/books/book.pb.go
+++ book_management/internal/pb/books/book.pb.go
@@ -287,6 +287,289 @@ func (x *BookItem) GetStatus() string {
 	return ""
 }
 
+type StockJournal struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id        uint64 `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	ItemId    uint64 `protobuf:"varint,2,opt,name=item_id,json=itemId,proto3" json:"item_id,omitempty"`
+	Status    string `protobuf:"bytes,3,opt,name=status,proto3" json:"status,omitempty"`
+	UserId    uint64 `protobuf:"varint,4,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	CreatedAt string `protobuf:"bytes,5,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
+	UpdatedAt string `protobuf:"bytes,6,opt,name=updated_at,json=updatedAt,proto3" json:"updated_at,omitempty"`
+	DeletedAt string `protobuf:"bytes,7,opt,name=deleted_at,json=deletedAt,proto3" json:"deleted_at,omitempty"`
+}
+
+func (x *StockJournal) Reset() {
+	*x = StockJournal{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StockJournal) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StockJournal) ProtoMessage() {}
+
+func (x *StockJournal) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StockJournal.ProtoReflect.Descriptor instead.
+func (*StockJournal) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *StockJournal) GetId() uint64 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+func (x *StockJournal) GetItemId() uint64 {
+	if x != nil {
+		return x.ItemId
+	}
+	return 0
+}
+
+func (x *StockJournal) GetStatus() string {
+	if x != nil {
+		return x.Status
+	}
+	return ""
+}
+
+func (x *StockJournal) GetUserId() uint64 {
+	if x != nil {
+		return x.UserId
+	}
+	return 0
+}
+
+func (x *StockJournal) GetCreatedAt() string {
+	if x != nil {
+		return x.CreatedAt
+	}
+	return ""
+}
+
+func (x *StockJournal) GetUpdatedAt() string {
+	if x != nil {
+		return x.UpdatedAt
+	}
+	return ""
+}
+
+func (x *StockJournal) GetDeletedAt() string {
+	if x != nil {
+		return x.DeletedAt
+	}
+	return ""
+}
+
+type StockJournals struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	StockJournal []*StockJournal `protobuf:"bytes,1,rep,name=stock_journal,json=stockJournal,proto3" json:"stock_journal,omitempty"`
+}
+
+func (x *StockJournals) Reset() {
+	*x = StockJournals{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StockJournals) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StockJournals) ProtoMessage() {}
+
+func (x *StockJournals) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StockJournals.ProtoReflect.Descriptor instead.
+func (*StockJournals) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *StockJournals) GetStockJournal() []*StockJournal {
+	if x != nil {
+		return x.StockJournal
+	}
+	return nil
+}
+
+type Id struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id uint64 `protobuf:"varint,1,opt,name=Id,proto3" json:"Id,omitempty"`
+}
+
+func (x *Id) Reset() {
+	*x = Id{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Id) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Id) ProtoMessage() {}
+
+func (x *Id) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Id.ProtoReflect.Descriptor instead.
+func (*Id) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *Id) GetId() uint64 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
+type Ids struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id []*Id `protobuf:"bytes,1,rep,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *Ids) Reset() {
+	*x = Ids{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Ids) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Ids) ProtoMessage() {}
+
+func (x *Ids) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Ids.ProtoReflect.Descriptor instead.
+func (*Ids) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *Ids) GetId() []*Id {
+	if x != nil {
+		return x.Id
+	}
+	return nil
+}
+
+type Status struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Status string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+}
+
+func (x *Status) Reset() {
+	*x = Status{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[8]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Status) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Status) ProtoMessage() {}
+
+func (x *Status) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[8]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Status.ProtoReflect.Descriptor instead.
+func (*Status) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{8}
+}
+
+func (x *Status) GetStatus() string {
+	if x != nil {
+		return x.Status
+	}
+	return ""
+}
+
 var File_book_proto protoreflect.FileDescriptor
 
 var file_book_proto_rawDesc = []byte{
@@ -318,12 +601,45 @@ var file_book_proto_rawDesc = []byte{
 	0x28, 0x04, 0x52, 0x02, 0x69, 0x64, 0x12, 0x17, 0x0a, 0x07, 0x62, 0x6f, 0x6f, 0x6b, 0x5f, 0x69,
 	0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x52, 0x06, 0x62, 0x6f, 0x6f, 0x6b, 0x49, 0x64, 0x12,
 	0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x32, 0x39, 0x0a, 0x0b, 0x42, 0x6f, 0x6f, 0x6b, 0x53,
-	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x2a, 0x0a, 0x08, 0x47, 0x65, 0x74, 0x42, 0x6f, 0x6f,
-	0x6b, 0x73, 0x12, 0x0e, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x45, 0x6d, 0x70,
-	0x74, 0x79, 0x1a, 0x0e, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x42, 0x6f, 0x6f,
-	0x6b, 0x73, 0x42, 0x13, 0x5a, 0x11, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x70,
-	0x62, 0x2f, 0x62, 0x6f, 0x6f, 0x6b, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0xc5, 0x01, 0x0a, 0x0c, 0x53, 0x74, 0x6f, 0x63,
+	0x6b, 0x4a, 0x6f, 0x75, 0x72, 0x6e, 0x61, 0x6c, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x04, 0x52, 0x02, 0x69, 0x64, 0x12, 0x17, 0x0a, 0x07, 0x69, 0x74, 0x65, 0x6d,
+	0x5f, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x52, 0x06, 0x69, 0x74, 0x65, 0x6d, 0x49,
+	0x64, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x03, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x17, 0x0a, 0x07, 0x75, 0x73, 0x65,
+	0x72, 0x5f, 0x69, 0x64, 0x18, 0x04, 0x20, 0x01, 0x28, 0x04, 0x52, 0x06, 0x75, 0x73, 0x65, 0x72,
+	0x49, 0x64, 0x12, 0x1d, 0x0a, 0x0a, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74,
+	0x18, 0x05, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x63, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x41,
+	0x74, 0x12, 0x1d, 0x0a, 0x0a, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74, 0x18,
+	0x06, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x75, 0x70, 0x64, 0x61, 0x74, 0x65, 0x64, 0x41, 0x74,
+	0x12, 0x1d, 0x0a, 0x0a, 0x64, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x64, 0x5f, 0x61, 0x74, 0x18, 0x07,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x64, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x64, 0x41, 0x74, 0x22,
+	0x4b, 0x0a, 0x0d, 0x53, 0x74, 0x6f, 0x63, 0x6b, 0x4a, 0x6f, 0x75, 0x72, 0x6e, 0x61, 0x6c, 0x73,
+	0x12, 0x3a, 0x0a, 0x0d, 0x73, 0x74, 0x6f, 0x63, 0x6b, 0x5f, 0x6a, 0x6f, 0x75, 0x72, 0x6e, 0x61,
+	0x6c, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x15, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70,
+	0x63, 0x2e, 0x53, 0x74, 0x6f, 0x63, 0x6b, 0x4a, 0x6f, 0x75, 0x72, 0x6e, 0x61, 0x6c, 0x52, 0x0c,
+	0x73, 0x74, 0x6f, 0x63, 0x6b, 0x4a, 0x6f, 0x75, 0x72, 0x6e, 0x61, 0x6c, 0x22, 0x14, 0x0a, 0x02,
+	0x49, 0x64, 0x12, 0x0e, 0x0a, 0x02, 0x49, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x02,
+	0x49, 0x64, 0x22, 0x22, 0x0a, 0x03, 0x49, 0x64, 0x73, 0x12, 0x1b, 0x0a, 0x02, 0x69, 0x64, 0x18,
+	0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x0b, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e,
+	0x49, 0x64, 0x52, 0x02, 0x69, 0x64, 0x22, 0x20, 0x0a, 0x06, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73,
+	0x12, 0x16, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x32, 0xca, 0x01, 0x0a, 0x0b, 0x42, 0x6f, 0x6f,
+	0x6b, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x2a, 0x0a, 0x08, 0x47, 0x65, 0x74, 0x42,
+	0x6f, 0x6f, 0x6b, 0x73, 0x12, 0x0e, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x45,
+	0x6d, 0x70, 0x74, 0x79, 0x1a, 0x0e, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x42,
+	0x6f, 0x6f, 0x6b, 0x73, 0x12, 0x25, 0x0a, 0x07, 0x47, 0x65, 0x74, 0x42, 0x6f, 0x6f, 0x6b, 0x12,
+	0x0b, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x49, 0x64, 0x1a, 0x0d, 0x2e, 0x67,
+	0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x42, 0x6f, 0x6f, 0x6b, 0x12, 0x33, 0x0a, 0x0b, 0x50,
+	0x6f, 0x73, 0x74, 0x42, 0x6f, 0x72, 0x72, 0x6f, 0x77, 0x73, 0x12, 0x0c, 0x2e, 0x67, 0x6f, 0x5f,
+	0x67, 0x72, 0x70, 0x63, 0x2e, 0x49, 0x64, 0x73, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72,
+	0x70, 0x63, 0x2e, 0x53, 0x74, 0x6f, 0x63, 0x6b, 0x4a, 0x6f, 0x75, 0x72, 0x6e, 0x61, 0x6c, 0x73,
+	0x12, 0x33, 0x0a, 0x0b, 0x50, 0x6f, 0x73, 0x74, 0x52, 0x65, 0x74, 0x75, 0x72, 0x6e, 0x73, 0x12,
+	0x0c, 0x2e, 0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x49, 0x64, 0x73, 0x1a, 0x16, 0x2e,
+	0x67, 0x6f, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x2e, 0x53, 0x74, 0x6f, 0x63, 0x6b, 0x4a, 0x6f, 0x75,
+	0x72, 0x6e, 0x61, 0x6c, 0x73, 0x42, 0x13, 0x5a, 0x11, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x6e, 0x61,
+	0x6c, 0x2f, 0x70, 0x62, 0x2f, 0x62, 0x6f, 0x6f, 0x6b, 0x73, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -338,23 +654,36 @@ func file_book_proto_rawDescGZIP() []byte {
 	return file_book_proto_rawDescData
 }
 
-var file_book_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_book_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
 var file_book_proto_goTypes = []any{
-	(*Empty)(nil),    // 0: go_grpc.Empty
-	(*Books)(nil),    // 1: go_grpc.Books
-	(*Book)(nil),     // 2: go_grpc.Book
-	(*BookItem)(nil), // 3: go_grpc.BookItem
+	(*Empty)(nil),         // 0: go_grpc.Empty
+	(*Books)(nil),         // 1: go_grpc.Books
+	(*Book)(nil),          // 2: go_grpc.Book
+	(*BookItem)(nil),      // 3: go_grpc.BookItem
+	(*StockJournal)(nil),  // 4: go_grpc.StockJournal
+	(*StockJournals)(nil), // 5: go_grpc.StockJournals
+	(*Id)(nil),            // 6: go_grpc.Id
+	(*Ids)(nil),           // 7: go_grpc.Ids
+	(*Status)(nil),        // 8: go_grpc.Status
 }
 var file_book_proto_depIdxs = []int32{
 	2, // 0: go_grpc.Books.book:type_name -> go_grpc.Book
 	3, // 1: go_grpc.Book.book_item:type_name -> go_grpc.BookItem
-	0, // 2: go_grpc.BookService.GetBooks:input_type -> go_grpc.Empty
-	1, // 3: go_grpc.BookService.GetBooks:output_type -> go_grpc.Books
-	3, // [3:4] is the sub-list for method output_type
-	2, // [2:3] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	4, // 2: go_grpc.StockJournals.stock_journal:type_name -> go_grpc.StockJournal
+	6, // 3: go_grpc.Ids.id:type_name -> go_grpc.Id
+	0, // 4: go_grpc.BookService.GetBooks:input_type -> go_grpc.Empty
+	6, // 5: go_grpc.BookService.GetBook:input_type -> go_grpc.Id
+	7, // 6: go_grpc.BookService.PostBorrows:input_type -> go_grpc.Ids
+	7, // 7: go_grpc.BookService.PostReturns:input_type -> go_grpc.Ids
+	1, // 8: go_grpc.BookService.GetBooks:output_type -> go_grpc.Books
+	2, // 9: go_grpc.BookService.GetBook:output_type -> go_grpc.Book
+	5, // 10: go_grpc.BookService.PostBorrows:output_type -> go_grpc.StockJournals
+	5, // 11: go_grpc.BookService.PostReturns:output_type -> go_grpc.StockJournals
+	8, // [8:12] is the sub-list for method output_type
+	4, // [4:8] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_book_proto_init() }
@@ -411,6 +740,66 @@ func file_book_proto_init() {
 				return nil
 			}
 		}
+		file_book_proto_msgTypes[4].Exporter = func(v any, i int) any {
+			switch v := v.(*StockJournal); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_book_proto_msgTypes[5].Exporter = func(v any, i int) any {
+			switch v := v.(*StockJournals); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_book_proto_msgTypes[6].Exporter = func(v any, i int) any {
+			switch v := v.(*Id); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_book_proto_msgTypes[7].Exporter = func(v any, i int) any {
+			switch v := v.(*Ids); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_book_proto_msgTypes[8].Exporter = func(v any, i int) any {
+			switch v := v.(*Status); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -418,7 +807,7 @@ func file_book_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_book_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   9,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
